380af1b09fefbab9e78c69f70913ce177e5c5b8e
mhenriette <mhenriette00@gnmail.com>
2023-02-25 23:56:16 +0200

    fix: footer logo size

--- project-a/components/sections/Footer.jsx
+++ project-a/components/sections/Footer.jsx
@@ -1,18 +1,25 @@
 import React from "react";
-
+import Image from "next/image";
+import logos from "../../src/data/logos"
 function Footer() {
-  return <div className="bg-[#1717e5] text-white">
-    <div className="mx-auto container pr-16 pl-32">
-        <ul className="flex items-center ">
+  return <div className="bg-[#1717e5] text-white py-6 px-8 flex justify-center">
+    <div className="mx-auto container flex justify-center gap-32">
+        <ul className="flex items-center">
             <li className="p-3">Contact</li>
-            <li className="p-3">Contact</li>
-            <li className="p-3">Contact</li>
-            <li className="p-3">Contact</li>
-            <li className="p-3">Contact</li>
-            <li className="p-3">Contact</li>
-            <li className="p-3">Contact</li>
-            <li className="p-3">Contact</li>
+            <li className="p-3">Press</li>
+            <li className="p-3">Privacy Policy</li>
+            <li className="p-3">Imprit</li>
+            <li className="p-3">FAQ</li>
+            <li className="p-3">Investor Portal</li>
+            <li className="p-3">SFDR</li>
         </ul>
+        <div className="flex gap-8 items-center">
+          {
+            logos.map((el)=>
+              <Image src={el.url} key={el.id} width={el.width} height={el.height}/>
+            )
+          }
+        </div>
 
     </div>
 
